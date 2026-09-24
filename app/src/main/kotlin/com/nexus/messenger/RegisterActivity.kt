a0a3419237f47f9ceadd438d0d5303daea510b89
Fifenya <mn5536348@gmail.com>
2026-09-24 13:13:38 +0600

    Update RegisterActivity.kt

--- app/src/main/kotlin/com/nexus/messenger/RegisterActivity.kt
+++ app/src/main/kotlin/com/nexus/messenger/RegisterActivity.kt
@@ -67,7 +67,7 @@ class RegisterActivity : Activity() {
                 setBackgroundResource(R.drawable.bg_input)
                 setPadding(dp(16), dp(14), dp(16), dp(14))
                 textSize = 16f
-                singleLine = true
+                maxLines = 1
             }
             root.addView(e, LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
                 bottomMargin = dp(12)
