56ea43aae302874fe3e23b4dee38339f639522db
Fifenya <mn5536348@gmail.com>
2026-09-24 17:20:23 +0600

    Update MainActivity.kt

--- app/src/main/kotlin/com/nexus/messenger/MainActivity.kt
+++ app/src/main/kotlin/com/nexus/messenger/MainActivity.kt
@@ -3,14 +3,25 @@ package com.nexus.messenger
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import com.nexus.messenger.data.Api
 import com.nexus.messenger.data.Store
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Store.init(applicationContext)
-        val target = if (Store.token != null) ChatsActivity::class.java else LoginActivity::class.java
-        startActivity(Intent(this, target))
-        finish()
+
+        // Сначала получаем актуальный URL сервера, потом маршрутизируем
+        Api.resolveServer {
+            runOnUiThread {
+                val target = if (Store.token != null) {
+                    ChatsActivity::class.java
+                } else {
+                    LoginActivity::class.java
+                }
+                startActivity(Intent(this, target))
+                finish()
+            }
+        }
     }
 }
